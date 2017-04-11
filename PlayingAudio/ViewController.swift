539bf476e1b38a99d824e6ae4a78da9d13d9f802
Oludemilade Raji <oludemilade@FREE.local>
2017-04-11 15:42:13 -0700

    Application that: 1. Plays sound 2. Plays music 3. Volume controller for sound 4. Volume controller for Music 5. Volume controller for Application

--- PlayingAudio/ViewController.swift
+++ PlayingAudio/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var soundEffect:AVAudioPlayer = AVAudioPlayer()
+    var musicEffect:AVAudioPlayer = AVAudioPlayer()
 
+    @IBOutlet var totalVolume: UISlider!
+    @IBOutlet var musicVolume: UISlider!
+    @IBOutlet var soundVolume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let soundFile = Bundle.main.path(forResource: "Sound", ofType: "wav")
+        let musicFile = Bundle.main.path(forResource: "Music", ofType: "mp3")
+        
+        do {
+            try soundEffect = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile!))
+            try musicEffect = AVAudioPlayer(contentsOf: URL(fileURLWithPath: musicFile!))
+        }
+        catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func soundEffectVolume(_ sender: Any) {
+        soundEffect.volume = soundVolume.value
+        
+        if (soundVolume.value > totalVolume.value)
+        {
+            soundVolume.value = totalVolume.value
+            soundEffect.volume = totalVolume.value
+        }
+        
+    }
+    @IBAction func musicEffectVolume(_ sender: Any) {
+        musicEffect.volume = musicVolume.value
+        
+        if (musicVolume.value > totalVolume.value)
+        {
+            musicVolume.value = totalVolume.value
+            musicEffect.volume = totalVolume.value
+        }
+    }
+    @IBAction func ApplicationVolume(_ sender: Any) {
+        soundEffect.volume = totalVolume.value
+        musicEffect.volume = totalVolume.value
+        
+        soundVolume.value = totalVolume.value
+        musicVolume.value = totalVolume.value
+    }
+    
+    @IBAction func playSoundEffect(_ sender: Any) {
+        soundEffect.play()
+    }
 
+    @IBAction func playMusic(_ sender: Any) {
+        musicEffect.play()
+    }
+    @IBAction func stopMusic(_ sender: Any) {
+        musicEffect.stop()
+        musicEffect.currentTime = 0
+    }
+    
+    @IBAction func pauseMusic(_ sender: Any) {
+        musicEffect.stop()
+    }
+    @IBAction func restartMusic(_ sender: Any) {
+        musicEffect.currentTime = 0
+    }
 }
 
